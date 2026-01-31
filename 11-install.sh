1f03c4dc8170cc5ad90bef8a4424904bd86750bc
an ga <rao2k42@gmail.com>
2026-01-31 13:47:51 -0500

    test

--- 11-install.sh
+++ 11-install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+USERID=$(id-u)
 
+echo "what is userid :$USERID"
+
+if ($USERID -ne 0); then
+echo "please run script with root user"
+fi
+
+echo "Instaling nginx"
 dnf install nginx -y
