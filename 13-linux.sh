426be47ff580cbc92ad1c19e07c509d13cd4897a
an ga <rao2k42@gmail.com>
2026-01-31 16:37:24 -0500

    test

--- 13-linux.sh
+++ 13-linux.sh
@@ -2,17 +2,18 @@
 
 USER_ID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
-LOGS_FILE=/var/log/shell-script/$0.log"
+LOGS_FILE="/var/log/shell-script/$0.log"
 
 mkdir -p $LOGS_FOLDER
 
+#BY Default shell will not execute
 VALIDATE(){
 if [ $1 -ne 0 ]; then
       echo "$2 ...Failure"
       exit 1
 else
      echo "$2 ... Success"
- fi
+fi
 }
 
 dnf install nginx -y &>> $LOGS_FILE
