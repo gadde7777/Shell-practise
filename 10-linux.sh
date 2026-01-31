2e2f7672ea854cbdb85d4053c23a8c3473831583
an ga <rao2k42@gmail.com>
2026-01-31 13:11:22 -0500

    test

--- 10-linux.sh
+++ 10-linux.sh
@@ -10,5 +10,9 @@ NUMBER=$1
 #-ne -> not equal
 
 if [ $NUMBER -gt 20 ]; then
-echo "Givem number: $NUMBER is greater than 25"
+echo "Givem number: $NUMBER is greater than 20"
+elif [ $NUMBER -eq 20 ];  then
+echo "Given Number "$NUMBER is equal to 20"
+else
+echo "Given Number ; $NUMBER is less than 20"
 fi
