138bdb65e4f4afba7950bebbd2e57a82c750a343
an ga <rao2k42@gmail.com>
2026-01-31 12:58:03 -0500

    test

---
+++ 10-linux.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+NUMBER=$1
+ 
+###################
+
+-gt -> greater than
+-lt -> less than
+-eq  -> equal to
+-ne -> not equal
+
+if [$NUMBER -gt 20]; then
+echo "Givem number : $NUMBER is greater than 25"
+fi
