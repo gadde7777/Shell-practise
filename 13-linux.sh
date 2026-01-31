e45d8b9b358581d203dbc18cd655e565807f41b5
an ga <rao2k42@gmail.com>
2026-01-31 16:48:28 -0500

    test

--- 13-linux.sh
+++ 13-linux.sh
@@ -9,10 +9,10 @@ mkdir -p $LOGS_FOLDER
 #BY Default shell will not execute
 VALIDATE(){
 if [ $1 -ne 0 ]; then
-      echo "$2 ...Failure"
+      echo "$2 ...Failure" | tee -a $LOGS_FILE
       exit 1
 else
-     echo "$2 ... Success"
+     echo "$2 ... Success" | tee -a $LOGS_FILE
 fi
 }
 
