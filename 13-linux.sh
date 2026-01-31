67081995c71dc9c51669606366a0445c38d7819a
an ga <rao2k42@gmail.com>
2026-01-31 16:30:02 -0500

    test

---
+++ 13-linux.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE=/var/log/shell-script/$0.log"
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+if [ $1 -ne 0 ]; then
+      echo "$2 ...Failure"
+      exit 1
+else
+     echo "$2 ... Success"
+ fi
+}
+
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE $? "Nginx Installation"
+
+dnf install mysql -y &>> $LOGS_FILE
+VALIDATE $? "Installing Mysql"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Installing nodejs"
