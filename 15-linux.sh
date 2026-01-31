e06e9efbcc862e1a1df51ac432a898fe978e5a4f
an ga <rao2k42@gmail.com>
2026-01-31 17:41:07 -0500

    test

---
+++ 15-linux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+mkdir -p $LOGS_FOLDER
+
+#BY Default shell will not execute
+VALIDATE(){
+if [ $1 -ne 0 ]; then
+      echo "$2 ...Failure" | tee -a $LOGS_FILE
+      exit 1
+else
+     echo "$2 ... Success" | tee -a $LOGS_FILE
+fi
+}
+
+for package in $@
+do
+   dnf install $package -y
+   VALIDATE $? "$package Installation"
+done
+
