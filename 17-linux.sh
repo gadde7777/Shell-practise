a1065aa72a22c0ab27b23702c7d003b7c362a743
an ga <rao2k42@gmail.com>
2026-01-31 19:20:06 -0500

    test

--- 17-linux.sh
+++ 17-linux.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
 set -e #ERR
+
 trap 'echo "There is an error in $LINENO, COMMAND: $BASH_COMMAND"' ERR
 
 USER_ID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+echo "please run script with root user"
+exit 1
+fi
 
 mkdir -p $LOGS_FOLDER
 
@@ -21,7 +27,6 @@ do
    if [ $? -ne 0 ]; then
      echo "$package not installed, installing now"
      dnf install $package -y &>>$LOGS_FILE
-   ## VALIDATE $? "$package Installation"
  else
     echo -e "$package already installed. $Y skipping $N"
     fi
